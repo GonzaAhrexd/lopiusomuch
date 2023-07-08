df0f5af761f1eb722aa8e5af501be074da05924a
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2023-07-08 15:01:55 -0300

    Horarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './vistaCelular.css'
 import Countdown from './components/Countdown'
 import LoveYou from './components/LoveYou'
 import Botones from './components/Botones'
@@ -24,7 +23,7 @@ function App() {
   }
 
   const flights = [
-    { name: "Asunción ✈️ Madrid", flightCode: 'AEA24', flightStartTime: new Date('2023-07-09T18:20:00-03:00').getTime(), flightEndTime: new Date('2023-07-10T06:25:00-03:00').getTime() },
+    { name: "Asunción ✈️ Madrid", flightCode: 'AEA24', flightStartTime: new Date('2023-07-09T19:20:00-03:00').getTime(), flightEndTime: new Date('2023-07-10T06:25:00-03:00').getTime() },
     { name: "Madrid ✈️ Roma", flightCode: 'ITY63', flightStartTime: new Date('2023-07-10T13:00:00-03:00').getTime(), flightEndTime: new Date('2023-07-10T15:25:00-03:00').getTime() },
     { name: "Roma ✈️ Bologna", flightCode: 'ITY1321', flightStartTime: new Date('2023-07-10T16:55:00-03:00').getTime(), flightEndTime: new Date('2023-07-10T17:50:00-03:00').getTime() },
   ];
