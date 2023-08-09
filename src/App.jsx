84f751e223ce0527a254a1d060a595c10726ba70
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2023-08-09 15:16:06 +0200

    Horarios actualizados

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,9 @@ function App() {
     { name: "Asunción ✈️ Madrid", flightCode: 'AEA24', flightStartTime: new Date('2023-07-09T19:20:00-03:00').getTime(), flightEndTime: new Date('2023-07-10T06:25:00-03:00').getTime() },
     { name: "Madrid ✈️ Roma", flightCode: 'ITY63', flightStartTime: new Date('2023-07-10T13:00:00-03:00').getTime(), flightEndTime: new Date('2023-07-10T15:25:00-03:00').getTime() },
     { name: "Roma ✈️ Bologna", flightCode: 'ITY1321', flightStartTime: new Date('2023-07-10T16:55:00-03:00').getTime(), flightEndTime: new Date('2023-07-10T17:50:00-03:00').getTime() },
+    { name: "Bologna ✈️ Roma", flightCode: 'ITY1312', flightStartTime: new Date('2023-08-15T06:30:00+02:00').getTime(), flightEndTime: new Date('2023-08-09T07:30:00+02:00').getTime() },
+    { name: "Roma ✈️ Madrid", flightCode: 'ITY60', flightStartTime: new Date('2023-08-15T14:35:00+02:00').getTime(), flightEndTime: new Date('2023-08-15T17:10:00+02:00').getTime() },
+    { name: "Madrid ✈️ Asunción ", flightCode: 'AEA23', flightStartTime: new Date('2023-08-15T23:45:00+02:00').getTime(), flightEndTime: new Date('2023-08-16T05:15:00-04:00').getTime() },
   ];
 
   return (
